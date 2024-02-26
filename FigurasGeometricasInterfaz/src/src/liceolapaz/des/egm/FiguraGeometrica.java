9edca26ee988368ab37e26d5309e642163d91caf
soyel <soyeloygarciamartinez@gmail.com>
2024-02-26 12:45:32 +0100

    cambios

--- FigurasGeometricasInterfaz/src/src/liceolapaz/des/egm/FiguraGeometrica.java
+++ FigurasGeometricasInterfaz/src/src/liceolapaz/des/egm/FiguraGeometrica.java
@@ -1,5 +1,6 @@
 package src.liceolapaz.des.egm;
 
+//en las interfaces la visibilidad por defecto es public, en las clases abstractas es package
 public interface FiguraGeometrica {
     public abstract double calcularArea();
     public abstract double calcularPerimetro();
--- FigurasGeometricasInterfaz/src/src/liceolapaz/des/egm/Principal.java
+++ FigurasGeometricasInterfaz/src/src/liceolapaz/des/egm/Principal.java
@@ -24,20 +24,79 @@ public class Principal {
                     figura = new Triangulo(primerLado, segundoLado, baseTriangulo, alturaTriangulo);
                     break;
                 case 2:
+                    pedirBaseRectangulo();
+                    double baseRectangulo = leerBaseRectangulo();
+                    pedirAlturaRectangulo();
+                    double alturaRectangulo = leerAlturaRectangulo();
+                    figura = new Rectangulo(baseRectangulo, alturaRectangulo);
+                    break;
                 case 3:
+                    pedirLadoCuadrado();
+                    double ladoCuadrado = leerLadoCuadrado();
+                    figura = new Cuadrado(ladoCuadrado);
+                    break;
                 case 4:
-
-
-
+                    pedirLadoPentagono();
+                    double ladoPentagono = leerLadoPentagono();
+                    pedirApotemaPentagono();
+                    double apotemaPentagono = leerApotemaPentagono();
+                    figura = new Pentagono(ladoPentagono, apotemaPentagono);
+                    break;
             }
             double area = figura.calcularArea();
-            escribirResultado(area);
+            double perimetro = figura.calcularPerimetro();
+            escribirResultado(area, perimetro);
         } while (true);
 
     }
 
-    private static void escribirResultado(double area) {
-        System.out.println("El area es: " + area);
+    private static double leerApotemaPentagono() {
+        Scanner teclado = new Scanner(System.in);
+        return teclado.nextInt();
+    }
+
+    private static void pedirApotemaPentagono() {
+        System.out.print("Escribe el valor de la apotema del pentagono: ");
+    }
+
+    private static double leerLadoPentagono() {
+        Scanner teclado = new Scanner(System.in);
+        return teclado.nextInt();
+    }
+
+    private static void pedirLadoPentagono() {
+        System.out.print("Escribe el valor de los lados del pentagono: ");
+    }
+
+    private static double leerLadoCuadrado() {
+        Scanner teclado = new Scanner(System.in);
+        return teclado.nextInt();
+    }
+
+    private static void pedirLadoCuadrado() {
+        System.out.print("Escribe el valor del lado del cuadrado: ");
+    }
+
+    private static double leerAlturaRectangulo() {
+        Scanner teclado = new Scanner(System.in);
+        return teclado.nextInt();
+    }
+
+    private static void pedirAlturaRectangulo() {
+        System.out.print("Escribe el valor de la altura del rectangulo: ");
+    }
+
+    private static double leerBaseRectangulo() {
+        Scanner teclado = new Scanner(System.in);
+        return teclado.nextInt();
+    }
+
+    private static void pedirBaseRectangulo() {
+        System.out.print("Escribe el valor de la base del rectangulo: ");
+    }
+
+    private static void escribirResultado(double area, double perimetro) {
+        System.out.println("El area es: " + area + " y el perimetro es: " + perimetro);
     }
 
     private static double leerSegundoLado() {
