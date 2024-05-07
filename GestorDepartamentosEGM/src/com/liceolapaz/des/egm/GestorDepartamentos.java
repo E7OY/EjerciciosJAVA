92d8edccc7a28e2decbc4182459cf678d910344a
EloyGarcia <soyeloygarciamartinez@gmail.com>
2024-05-07 18:48:21 +0200

    cambios

--- GestorDepartamentosEGM/src/com/liceolapaz/des/egm/GestorDepartamentos.java
+++ GestorDepartamentosEGM/src/com/liceolapaz/des/egm/GestorDepartamentos.java
@@ -1,8 +1,6 @@
 package com.liceolapaz.des.egm;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -16,6 +14,10 @@ public class GestorDepartamentos {
             escribirMenu();
             int opcion = leerEntero();
             switch (opcion) {
+
+                case 0:
+                    System.exit(0);
+                    break;
 
                 case 1:
                     pedirRuta();
@@ -33,10 +35,70 @@ public class GestorDepartamentos {
                         System.out.println(departamentos);
                     }
                     break;
+                case 3:
+                    departamento = crearDepartamento();
+                    num_depto = departamento.getNum_depto();
+                    if (departamentos.containsKey(num_depto)) {
+                        departamentos.replace(num_depto, departamento);
+                    } else {
+                        System.err.println("No existe un departamento con el numero de departamento: " + num_depto);
+                    }
+                    break;
 
+                case 4:
+                    pedirDato("Numero de departamento");
+                    String num_depto1 = leerString();
+                    num_depto = 0;
+                    try {
+                        num_depto = Integer.parseInt(num_depto1);
+                    } catch (NumberFormatException e){
+                        System.err.println("El número de departamento tiene que ser un número entero");
+                        break;
+                    }
+                    if (!departamentos.containsKey(num_depto)) {
+                        System.err.println("No existe un departamento con esa clave!!");
+                    } else {
+                        departamentos.remove(num_depto);
+                    }
+                    break;
+
+                case 5:
+                    pedirRutaExportar();
+                    String ruta1 = leerString();
+                    crearFichero(ruta1);
+                    break;
             }
-            }
+        }
     }
+
+    private static void crearFichero(String ruta1) {
+        try {
+            FileWriter fw = new FileWriter(ruta1);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter pw = new PrintWriter(bw);
+            String linea = leerString();
+            for (Departamento departamento : departamentos.values()) {
+                linea = "";
+                linea += departamento.getNum_depto() + ";";
+                linea += departamento.getNombre() + ";";
+                linea += departamento.getNombre_corto() + ";";
+                linea += departamento. getPlanta() + ";";
+                linea+= departamento.getCif_director() + ";";
+                linea += departamento.getCorreo_e() + ";";
+                pw.println(linea);
+            }
+
+            pw.close();
+            bw.close();
+            fw.close();
+        } catch (IOException e) {
+            System.err.println("Error al escribir el fichero");        }
+    }
+
+    private static void pedirRutaExportar() {
+        System.out.println("Escribe la ruta del fichero donde quieres exportar los departamentos: ");
+    }
+
 
     private static Departamento crearDepartamento() {
         Departamento departamento = null;
@@ -44,7 +106,12 @@ public class GestorDepartamentos {
         pedirDato("Numero de departamento");
         String numero = leerString();
         int num_depto = 0;
-        num_depto = Integer.parseInt(numero);
+        try {
+            num_depto = Integer.parseInt(numero);
+        } catch (NumberFormatException e) {
+            System.err.println("El número de departamento tiene que ser un número entero");
+            return null;
+        }
 
         pedirDato("Nombre");
         String nombre = leerString();
@@ -55,7 +122,12 @@ public class GestorDepartamentos {
         pedirDato("Planta");
         String planta = leerString();
         int numPlanta = 0;
-        numPlanta = Integer.parseInt(planta);
+        try {
+            numPlanta = Integer.parseInt(planta);
+        } catch (NumberFormatException e) {
+            System.err.println("La planta tiene que ser un número entero");
+            return null;
+        }
 
         pedirDato("cif director");
         String cifDirector = leerString();
@@ -63,7 +135,7 @@ public class GestorDepartamentos {
         pedirDato("correo empresa");
         String correoEmpresa = leerString();
 
-         departamento = new Departamento(num_depto, nombre, nombreCorto, numPlanta, cifDirector, correoEmpresa);
+        departamento = new Departamento(num_depto, nombre, nombreCorto, numPlanta, cifDirector, correoEmpresa);
 
         return departamento;
 
@@ -98,7 +170,7 @@ public class GestorDepartamentos {
                         num_depto = 0;
                         num_depto = Integer.parseInt(num_deptoTexto);
                     } catch (NumberFormatException e) {
-                        System.out.println("El numero de departamento tiene que ser un número enter.");
+                        System.err.println("El numero de departamento tiene que ser un número enter.");
                         return;
                     }
 
@@ -112,7 +184,7 @@ public class GestorDepartamentos {
                         planta = 0;
                         planta = Integer.parseInt(plantaTexto);
                     } catch (NumberFormatException e) {
-                        System.out.println("El numero de planta tiene que ser un número entero.");
+                        System.err.println("El numero de planta tiene que ser un número entero.");
                         return;
                     }
 
